78390f9cdf6620e6485fd0b59a38028e65b2be19
hase218 <chju23041@g.nihon-u.ac.jp>
2024-12-29 10:32:48 +0900

    結果を表示

--- src/components/Pages/Result.jsx
+++ src/components/Pages/Result.jsx
@@ -1,24 +1,31 @@
+import { useState } from "react";
 import { useLocation } from "react-router-dom";
 
 export default function Result() {
     const location = useLocation();
     const payload = location.state;
     //location.state.imgDataがうごかないのはなぜ
-    const img = payload.imgData;
-    const category = payload.categoryData;
-    
-    const maxCategory = Object.keys(category).reduce((maxKey, key) =>{
-        if (category[key] > (category[maxKey] || 0)) { //maxの初期値0
-            return key;
-        } else {
-            return maxKey;
-        } 
-    }, "");
+    const [img, setImg] = useState(payload.imgData);
+    const [category, setCategory] = useState(payload.categoryData);
+
+    const [maxCategory, setMaxCategory] = useState(() => {
+        return Object.keys(category).reduce((maxKey, key) => { //Array.prototype.reduce()
+            if (category[key] > (category[maxKey] || 0)) { //maxの初期値0
+                return key;
+            } else {
+                return maxKey;
+            }
+        }, "");
+    });
 
     // console.log(img);
     console.log(category);
     console.log(maxCategory);
-    return(
-        <div>result</div>
+    return (
+        <div>
+            あなたがえらんだのは
+            <p>{maxCategory}</p>
+        </div>
+
     );
 }
